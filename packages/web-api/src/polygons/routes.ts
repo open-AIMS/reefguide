07c1d8cdb3b38a0752da510d387b5cb6f2145258
Arlo White <a.white@aims.gov.au>
2025-10-23 13:57:53 +1000

    organize files

--- packages/web-api/src/polygons/routes.ts
+++ packages/web-api/src/polygons/routes.ts
@@ -18,6 +18,7 @@ import { assertUserHasRoleMiddleware, userIsAdmin } from '../auth/utils';
 import { InternalServerError, NotFoundException, UnauthorizedException } from '../exceptions';
 import { userHasProjectAccess } from '../util';
 import tokml from '@maphubs/tokml';
+import { polygonsToGeoJSON } from './polygons-conversion';
 
 require('express-async-errors');
 
@@ -90,47 +91,6 @@ router.get(
     }
   }
 );
-
-/**
- * Format polygons as single text value
- * @param polygon
- */
-function formatNotes(polygon: PolygonWithRelations): string {
-  const dateFormat = new Intl.DateTimeFormat('en-AU', {
-    dateStyle: 'medium',
-    timeStyle: 'short'
-  });
-  return polygon.notes
-    .map(note => {
-      return `# ${dateFormat.format(note.created_at)} \u2014 ${note.user.email}\n${note.content.trim()}`;
-    })
-    .join('\n\n');
-}
-
-/**
- * Wraps the polygons in a GeoJSON feature collection.
- * @param polygons
- */
-function polygonsToGeoJSON(polygons: PolygonWithRelations[]): any {
-  return {
-    type: 'FeatureCollection',
-    // TODO add bbox? https://datatracker.ietf.org/doc/html/rfc7946#section-5
-    features: polygons.map(p => {
-      return {
-        type: 'Feature',
-        // TODO what date format?
-        // TODO add comments
-        properties: {
-          fid: p.id,
-          createdAt: p.created_at.toISOString(),
-          createdBy: p.user.email,
-          notes: formatNotes(p)
-        },
-        geometry: p.polygon
-      };
-    })
-  };
-}
 
 /**
  * Get all polygons based on user permissions and filters
